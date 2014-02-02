5b61ffa6f549df432ece13ffc25b1609c31ae487
Dan Stepchew <dstepchew@gmail.com>
2014-02-01 17:04:41 -0800

    Update show view, add glyphicons, and change root path

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ ZoomApp::Application.routes.draw do
   resources :pins
 
   devise_for :users
-  root "pages#home"
+  root "pins#index"
   get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
