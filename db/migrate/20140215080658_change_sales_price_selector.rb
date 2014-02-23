aebb14a56cdfa06f305d77d49fc1c616ced0b0f7
Dan Stepchew <dstepchew@gmail.com>
2014-02-22 16:29:37 -0800

    altered migration for Heroku

--- db/migrate/20140215080658_change_sales_price_selector.rb
+++ db/migrate/20140215080658_change_sales_price_selector.rb
@@ -1,6 +1,6 @@
 class ChangeSalesPriceSelector < ActiveRecord::Migration
   def self.up
-    change_column :stores, :sales_price_selector, :boolean
+    change_column :stores, :sales_price_selector, 'boolean USING CAST(sales_price_selector AS boolean)'
   end
 
   def self.down
