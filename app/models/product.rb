be3315876e75ee1a2b865d2b0a62c9aec2ef06a4
Dan Stepchew <dstepchew@gmail.com>
2014-04-19 14:18:19 -0700

    removed store update

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,7 +10,7 @@
 			accepts_nested_attributes_for :pins, :allow_destroy => true
       self.per_page = 6
 
-      after_update :update_store
+      #after_update :update_store
 
       def update_store
         self.store.update_attribute(:status, self.status)
